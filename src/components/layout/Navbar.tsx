59e7d28ca357003a11fa291a8bf3d1bf650d1dd7
Baha Erdoğan <baha-erdogan@hotmail.com>
2026-01-08 21:33:08 +0300

    frontendupdates

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { Menu, X } from "lucide-react";
-import dikeyLogo from "@/assets/horizontalLogoNoBG.png";
+import dikeyLogo from "@/assets/dikey-logo.png";
 import LanguageSwitch from "@/components/LanguageSwitch";
 import { useLanguage } from "@/context/LanguageContext";
 
