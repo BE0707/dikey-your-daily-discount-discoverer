b925972624d47763a46c12169b7ea885af178fb1
Baha Erdoğan <baha-erdogan@hotmail.com>
2026-01-08 21:54:13 +0300

    update indexhtml for pages

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,19 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+// Handle GitHub Pages 404 redirect
+// https://github.com/rafgraph/spa-github-pages
+(function(l) {
+  if (l.search[1] === '/' ) {
+    var decoded = l.search.slice(1).split('&').map(function(s) { 
+      return s.replace(/~and~/g, '&')
+    }).join('?');
+    window.history.replaceState(null, null,
+        l.pathname.slice(0, -1) + decoded + l.hash
+    );
+  }
+}(window.location))
+
 createRoot(document.getElementById("root")!).render(<App />);
 
 if ("serviceWorker" in navigator) {
